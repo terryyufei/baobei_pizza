653362bcf6993e0ea3db6b3d09f54a89b1869d53
Terry Wambui <shikuwambui@gmail.com>
2024-01-31 21:41:59 +0300

    Initial commit

---
+++ resources/views/pizzas.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Pizza List
+            </div>
+            <p>{{ $type }} - {{ $base }} - {{ $price }} </p>
+            
+
+
+
+
+            {{--  
+
+            @if ($price > 15)
+                <p>this pizza is too expensive</p>
+            @elseif($price < 5)
+                <p>this pizza is cheap</p>
+            @else
+                <p>This Pizza is normally priced</p>
+            @endif
+
+
+            
+            @unless ($base == 'cheesy crust')
+                <p>You don't have a cheesy crust</p>
+            @endunless
+
+            
+            @php
+                $name = 'Gibson';
+                echo($name);
+            @endphp
+            --}}
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/pizzas', function () {
+    $pizza = [
+        'type' => 'hawaiian',
+        'base'=> 'cheesy crust',
+        'price'=> '10',
+    ];
+    return view('pizzas', $pizza);
+});
